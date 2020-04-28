eb5749a632125a68025488c15b4d2a6cc1b8f116
KF7EEL <57960556+kf7eel@users.noreply.github.com>
2020-04-28 14:48:55 -0700

    Create view.py

---
+++ cgi-bin/view.py
@@ -0,0 +1,58 @@
+#!/usr/bin/python3.7
+
+###############################################################################
+#   Copyright (C) 2020 Eric Craw, KF7EEL <kf7eel@qsl.net>
+#
+#   This program is free software; you can redistribute it and/or modify
+#   it under the terms of the GNU General Public License as published by
+#   the Free Software Foundation; either version 3 of the License, or
+#   (at your option) any later version.
+#
+#   This program is distributed in the hope that it will be useful,
+#   but WITHOUT ANY WARRANTY; without even the implied warranty of
+#   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#   GNU General Public License for more details.
+##
+#   You should have received a copy of the GNU General Public License
+#   along with this program; if not, write to the Free Software Foundation,
+#   Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301  USA
+###############################################################################
+
+# Modified 04-28-2020, by Eric, KF7EEL
+
+# Generates HTML page to view rules in BRIDGES dictionary.
+
+import cgi, cgitb, sys, os
+#from importlib import import_module
+#config = import_module("../config.py")
+#my_module.my_function()
+
+#sys.path
+#sys.path.append('/home/user/.../')
+#sys.path.append('..')
+
+PACKAGE_PARENT = '..'
+SCRIPT_DIR = os.path.dirname(os.path.realpath(os.path.join(os.getcwd(), os.path.expanduser(__file__))))
+sys.path.append(os.path.normpath(os.path.join(SCRIPT_DIR, PACKAGE_PARENT)))
+
+from config import *
+
+# Rule viewer
+
+from core import *
+from rules import *
+
+
+print(header)
+
+print('''
+<h2 style="text-align: center;"><strong>Current bridges:</strong></h2>
+
+''')
+
+html_bridge_view_all()
+print(' <tbody>')
+
+print(' </tbody>')
+print('</table>')
+print(footer)
